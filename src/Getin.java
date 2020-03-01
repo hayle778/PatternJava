ddeafc4317602ab6f1bb82deb3d826b4c212a5dc
yourUsername <yourEmail@domain.com>
2020-03-01 15:36:44 -0500

    gg

---
+++ src/Getin.java
@@ -0,0 +1,31 @@
+public class Getin {
+public static void main(String[]args) {
+
+  String a = "J";
+  String b = "a";
+  String c = "v";
+
+
+
+    System.out.println("       jj " +"   a    " +  "v  "    + "     v"+ "       a"              );
+    System.out.println("       jj " +"  a a   " +  " v  "   + "   v"  + "      a  a "          );
+    System.out.println("jj     jj "+ " aaaaaa " +  "  v  "  + " v"    + "      aaaaaaa"         );
+    System.out.println("  jjjjjj  "+ "a      a" +  "    vv  "         + "   a         a   "      );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+}
